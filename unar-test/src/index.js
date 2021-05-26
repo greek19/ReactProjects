73b3f2eb3af975002b6f9930d1d7e1a11c0413a1
Paolo <p.marchitto@exolab.it>
2021-05-26 17:00:37 +0200

    creazione rootReducer e installazione redux js/toolkit

--- unar-test/src/index.js
+++ unar-test/src/index.js
@@ -3,10 +3,18 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import { Provider } from 'react-redux';
+import rootReducer  from '../src/store/reducers/rootReducer';
+
+const store = createStore(rootReducer,applyMiddleware(thunk));
 
 ReactDOM.render(
   <React.StrictMode>
+    <Provider store={store} >
     <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
